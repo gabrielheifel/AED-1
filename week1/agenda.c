3c3364c9f74871f8b3a205aa6ddbefd89142160d
GHeifel <gabrielheifel@gmail.com>
2020-10-17 18:57:05 -0300

    agenda ok

--- week1/agenda.c
+++ week1/agenda.c
@@ -4,11 +4,13 @@
 
 typedef struct {
    char nome[30];
-   char telefone[20];
+   char tel[20];
 }Contato;
 
 typedef struct {
-   int cont, op, id, tambuffer;
+   int op, cont, tambuffer, i;
+   char find[30];
+   Contato *quit;      //ponteiro para contato
 }Variaveis;
 
 // Declaração dos ponteiros
@@ -17,51 +19,100 @@ Contato *pContato;
 Variaveis *var;
 
 void addContato() {
-   Contato *newcontato;   //nova pessoa
 
-   var->cont += 1;        //contador 
-   var->tambuffer = (sizeof(Variaveis)) + (var->id*sizeof(Contato));
-   pBuffer = realloc(pBuffer, var->tambuffer);
+   var->cont += 1;        //contador para ver quantos contatos adicionados
+   //Novo tambuffer com o numero dos contatos adicionados
+   var->tambuffer = (sizeof(Variaveis)) + (var->cont*sizeof(Contato)); 
+   pBuffer = realloc(pBuffer, var->tambuffer);     //realoca o novo tamanho
    if(pBuffer==NULL){
       printf("Memoria insuficiente!\n");
       exit(1);
    }
-   var = pBuffer;
-   newcontato = pBuffer + (var->tambuffer - sizeof(Contato));
+   var = pBuffer;          //variaveis aponta para o Buffer 
+   //contato recebe nova posiçao de memoria do buffer + tamanho - 1 contato q vai ser adicionado
+   pContato = pBuffer + (var->tambuffer - sizeof(Contato));    
    
    getchar();
-   printf("Novo contato\n");
+   printf("==== Novo contato ====\n");
    printf("Nome: ");
-   fgets(newcontato->nome, 30, stdin);
+   fgets(pContato->nome, 30, stdin);
    printf("Telefone: ");
-   fgets(newcontato->telefone, 20, stdin);   
+   fgets(pContato->tel, 20, stdin);   
 
-   printf("Feito\n");
+   printf("Feito!\n");
+}
+
+void delete() {
+   printf("==== Remover contato ====\n");
+   getchar();
+   fgets(var->find, 30, stdin);
+
+   pContato = pBuffer + (sizeof(Variaveis)); //Contato aponta para o buffer
+   var->i = 0;    //contador para percorrer o buffer
+   //verificar se exite o nome nos contatos
+   while(strcmp(var->find, pContato->nome)!=0 && var->i < var->cont){
+      pContato = (Contato*) pContato+1;   //recebe endereço do prox contato
+      var->i +=1;
+   }
+   if(var->i == var->cont){
+      printf("Nome não existe na agenda.\n");
+   }
+   else{
+      //verificar qual nome deve ser apagado
+      //var ponteiro/contato aponta para o contato encontrado
+      var->quit = (Contato*) pContato+1;
+
+      //sobrescreve os contatos que estão depois do contato encontrado
+      while(var->i < var->cont){
+         strcpy(pContato->nome, var->quit->nome);         
+         strcpy(pContato->tel, var->quit->tel);         
+         pContato = (Contato*) pContato+1;
+         var->quit = (Contato*) var->quit+1;
+         var->i+=1;
+      }
+      //diminui o tamanho do buffer
+      var->cont-=1;
+      var->tambuffer = (sizeof(Variaveis)) + (var->cont * sizeof(Contato));
+      pContato == pBuffer + (var->tambuffer - sizeof(Contato));
+
+      printf("Contato removido!\n");
+   }
+}
+
+void list() {
+   printf("==== Listar contatos ====\n");
+   pContato = pBuffer + (sizeof(Variaveis));
+
+   for(var->i = 0; var->i < var->cont; (var->i)++){
+      printf("=== Contato %d ===\n", var->i);
+      printf("Nome: %s", pContato[var->i].nome);
+      printf("Telefone: %s\n", pContato[var->i].tel);
+   }
 }
 
 void menu() {
 
-   for( ; ; ){
+   while(var->op!=4){
+      printf("\nDigite:\n");
       printf("1) Adicionar contato\n");
-      // printf("2) Apagar contato\n");
-      // printf("3) Listar\n");
+      printf("2) Apagar contato\n");
+      printf("3) Listar contatos\n");
       printf("4) Sair\n");
       printf("Opcao: ");
       scanf("%d", &var->op);
 
       switch(var->op) {
          case 1: addContato(); break;
-         // case 2: delete(); break;
-         // case 3: list(); break;
-         case 4: exit(1);
-         default: printf("Apenas 1 ou 4\n"); break;
+         case 2: delete(); break;
+         case 3: list(); break;
+         default: break;
       }
    }
 }
 
 int main() {
    
-   pBuffer = malloc(sizeof(Variaveis))+sizeof(Contato);
+   pBuffer = malloc(sizeof(Variaveis));
    if(pBuffer==NULL){
       printf("Falta de memoria\n");
       exit(1);
