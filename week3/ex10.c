10c3558d6b420168005d32009a2a60a5c2557c10
GHeifel <gabrielheifel@gmail.com>
2020-11-01 19:52:37 -0300

    ex10

--- week3/ex10.c
+++ week3/ex10.c
@@ -10,26 +10,31 @@
 // b) Calcule o valor de j para o valor de entrada n = 10;
 // c) Implemente uma função similar utilizando 2 loops for;
 
-// a) A função é um contador, retorna o valor de j, que 
-//    é o mesmo valor de n, caso o n seja diferente de 0.
-//    E i é o dobro de j menos 1.
+// a) A função é retorna a raiz de n.
 
-// b) j = 10
+// b) j = 100
 
 // c)
 #include <stdio.h>
 
 int F1(unsigned int n) {
    if(n==0) return 0;
-   int i, j;
-   for(i=1; i<2 *n-1; i+=2);
-   for(j=1; j<n; j+=1);
-   printf("i = %d | ", i);
+   int i, j=0, k;
+   // for ( i = j = 1; i<2 *n-1 ; i+=2, j +=i);
+   for(i=0; i<n; i++){
+      for(k=0; k<n; k++){
+         j++;
+      }
+   }
+   // for(i=j=1; i<n; i++, j++){
+   //    for(k=0; k<n; k++, j++);
+   // }
+
    return j;
 }
 
 int main() {
-   int n=10, j;
+   int n=4, j;
    
    j = F1(n);
    printf("j = %d\n", j);
